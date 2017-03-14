f5deea35742a1673d0ce3682795cf0e1d8c1af52
David-Loughnane <david.loughnane1@gmail.com>
2017-03-14 11:14:57 +0000

    move sorting to RDD

--- examples/word-count.py
+++ examples/word-count.py
@@ -32,19 +32,36 @@ def normalise_words(text):
 
 book = sc.textFile('file:///Users/davidloughnane/Documents/training/udemy_pyspark/data/book.txt')
 
-words = book.flatMap(normalise_words)
 
+
+words = book.flatMap(normalise_words)
 word_count = words.countByValue()
 
 # sort on the value of the dict, order from largest to smallest
+# this sorts on the python object, we want to sort on the RDD
 sorted_word_count = collections.OrderedDict(sorted(word_count.items(), key=itemgetter(1), reverse=True))
 
+print('Sorted python object')
 for i, (word, count) in enumerate(sorted_word_count.items()):
 	if i == 20:
 		break
 	clean_word = word.encode('ascii', 'ignore')
 	print('{0}: {1}'.format(word, count))
 
+
+# implement countByValue
+word_count_rdd = words.map(lambda x: (x,1)).reduceByKey(lambda x, y: x + y)
+# flip (word. count) tp (count, word) pairs, then sort by key
+word_counts_sorted_rdd = word_count_rdd.map(lambda x: (x[1],x[0])).sortByKey()
+# bring rdd into python object
+word_counts_sorted = word_counts_sorted_rdd.collect()
+
+print('\nSorted RDD')
+for i, (count, word) in enumerate(word_counts_sorted):
+	if i == 20:
+		break
+	clean_word = word.encode('ascii', 'ignore')
+	print('{0}: {1}'.format(word, count))
 
 
 
