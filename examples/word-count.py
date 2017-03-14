4c06e16da9b3068bc2ed43de8d9ad6c232705df4
David-Loughnane <david.loughnane1@gmail.com>
2017-03-14 10:56:53 +0000

    regex to strip out punctuation, convert all to lower case

--- examples/word-count.py
+++ examples/word-count.py
@@ -1,11 +1,11 @@
 # map Vs flatmap
 from pyspark import SparkConf, SparkContext
+import re
 import collections
 from operator import itemgetter
 
 conf = SparkConf().setMaster('local').setAppName('wordCount')
 sc = SparkContext(conf = conf)
-
 
 '''
 lines = sc.textFile('file:///Users/davidloughnane/Documents/training/udemy_pyspark/data/quick-brown.txt')
@@ -26,17 +26,25 @@ for result in result2:
 	print(result, ' ')
 '''
 
+def normalise_words(text):
+	''' regex - break up text based on words (strip out punctuation), transform all to lower case'''
+	return re.compile(r'\W+', re.UNICODE).split(text.lower())
 
 book = sc.textFile('file:///Users/davidloughnane/Documents/training/udemy_pyspark/data/book.txt')
 
-words = book.flatMap(lambda x: x.split())
+words = book.flatMap(normalise_words)
+
 word_count = words.countByValue()
+
+# sort on the value of the dict, order from largest to smallest
 sorted_word_count = collections.OrderedDict(sorted(word_count.items(), key=itemgetter(1), reverse=True))
 
-
-for word, count in sorted_word_count.items():
+for i, (word, count) in enumerate(sorted_word_count.items()):
+	if i == 20:
+		break
 	clean_word = word.encode('ascii', 'ignore')
 	print('{0}: {1}'.format(word, count))
+
 
 
 
